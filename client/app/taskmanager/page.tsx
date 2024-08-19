d356e0d817dae71448e0602933145f8f351176fc
voidCounter <rohitroy7014@gmail.com>
2024-08-20 03:06:03 +0600

    Added sidebar, routing

--- client/app/taskmanager/page.tsx
+++ client/app/taskmanager/page.tsx
@@ -1,6 +1,7 @@
-export default function Home() {
+export default function TaskManager() {
   return (
     <main className="flex min-h-screen flex-col text-6xl items-center justify-between p-24">
+      Producing Heating tasks
     </main>
   );
 }
---
+++ client/components/sidebar.tsx
@@ -0,0 +1,9 @@
+import {Nav} from "@/components/nav";
+
+export default function Sidebar() {
+    return (
+        <div className="p-0.5">
+            <Nav></Nav>
+        </div>
+    );
+}
