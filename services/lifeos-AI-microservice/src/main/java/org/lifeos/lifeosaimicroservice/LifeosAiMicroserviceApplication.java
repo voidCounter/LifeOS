a3d81f9f3a36b84e68d7d6c7d4dfa0b8c2d4a4d9
voidCounter <rohitroy7014@gmail.com>
2024-08-25 01:04:24 +0600

    Added Eureka server, setup eureka client at each microservice

--- services/lifeos-AI-microservice/src/main/java/org/lifeos/lifeosaimicroservice/LifeosAiMicroserviceApplication.java
+++ services/lifeos-AI-microservice/src/main/java/org/lifeos/lifeosaimicroservice/LifeosAiMicroserviceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAu
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
         PgVectorStoreAutoConfiguration.class})
+@EnableDiscoveryClient
 public class LifeosAiMicroserviceApplication {
 
     public static void main(String[] args) {
---
+++ services/lifeos-service-registry/src/test/java/org/lifeos/lifeosserviceregistry/LifeosServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lifeos.lifeosserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeosServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
