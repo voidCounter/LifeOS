4a72971c683c50c60306d458800e07b9a44d1a59
voidCounter <rohitroy7014@gmail.com>
2024-08-26 02:11:26 +0600

    Eureka working, setup cloud gateway, moved jwt authentication from user service to gateway

--- services/lifeos-gateway/src/main/java/org/lifeos/lifeosgateway/service/JWTService.java
+++ services/lifeos-gateway/src/main/java/org/lifeos/lifeosgateway/service/JWTService.java
@@ -1,19 +1,15 @@
-package org.lifeos.userservice.service;
+package org.lifeos.lifeosgateway.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.lifeos.userservice.model.CustomUserDetails;
+import org.lifeos.lifeosgateway.model.CustomUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -22,11 +18,9 @@ import java.util.Map;
 @Service
 public class JWTService {
     private final CustomUserDetailsService userDetailsService;
-    private final CustomUserDetailsService customUserDetailsService;
 
-    public JWTService(CustomUserDetailsService userDetailsService, CustomUserDetailsService customUserDetailsService) {
+    public JWTService(CustomUserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
-        this.customUserDetailsService = customUserDetailsService;
     }
 
     private static final Logger log = LoggerFactory.getLogger(JWTService.class);
@@ -54,5 +48,34 @@ public class JWTService {
     private SecretKey getKey() {
         byte[] decodedKey = Base64.getDecoder().decode(secretKey);
         return Keys.hmacShaKeyFor(decodedKey);
+    }
+
+    public String extractUserId(String jwt) {
+        Claims claims = extractAllClaims(jwt);
+        return claims.getSubject();
+    }
+
+    public Date extractExpiration(String jwt) {
+        Claims claims = extractAllClaims(jwt);
+        return claims.getExpiration();
+    }
+
+    public Claims extractAllClaims(String jwt) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+
+    }
+
+    public boolean validateToken(String jwt, CustomUserDetails userDetails) {
+        String extractUserId = extractUserId(jwt);
+        String userId = userDetails.getUserId();
+        return extractUserId.equals(userId) && !isTokenExpired(jwt);
+    }
+
+    private boolean isTokenExpired(String jwt) {
+        return extractExpiration(jwt).before(new Date());
     }
 }
