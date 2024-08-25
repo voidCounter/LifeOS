b0126e989ffb3de7f69fbc07a7035dbbde6106ef
tajbirahmed <tajbir26@gmail.com>
2024-08-25 17:44:47 +0600

    added pathway service, pathway ai generation

---
+++ services/lifeos-AI-microservice/src/main/java/org/lifeos/lifeosaimicroservice/model/StageType.java
@@ -0,0 +1,30 @@
+package org.lifeos.lifeosaimicroservice.model;
+
+public enum StageType {
+    ROADMAP,
+    MILESTONE,
+    MODULE,
+    TASK,
+    QUIZ,
+    PROJECT;
+
+    public String getValue() {
+        return switch (this){
+            case QUIZ -> "QUIZ";
+            case ROADMAP -> "ROADMAP";
+            case MILESTONE -> "MILESTONE";
+            case MODULE -> "MODULE";
+            case TASK -> "TASK";
+            case PROJECT -> "PROJECT";
+        };
+    }
+
+    public static StageType fromValue(String value) {
+        for (StageType stageType : StageType.values()) {
+            if (stageType.getValue().equalsIgnoreCase(value)) {
+                return stageType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown value: " + value);
+    }
+}
---
+++ services/lifeos-pathway-microservice/src/main/java/org/lifeos/pathway/model/StageType.java
@@ -0,0 +1,31 @@
+package org.lifeos.pathway.model;
+
+
+public enum StageType {
+    ROADMAP,
+    MILESTONE,
+    MODULE,
+    TASK,
+    QUIZ,
+    PROJECT;
+
+    public String getValue() {
+        return switch (this){
+            case QUIZ -> "QUIZ";
+            case ROADMAP -> "ROADMAP";
+            case MILESTONE -> "MILESTONE";
+            case MODULE -> "MODULE";
+            case TASK -> "TASK";
+            case PROJECT -> "PROJECT";
+        };
+    }
+
+    public static StageType fromValue(String value) {
+        for (StageType stageType : StageType.values()) {
+            if (stageType.getValue().equalsIgnoreCase(value)) {
+                return stageType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown value: " + value);
+    }
+}
