0d7613a401fc36d915e6c752607aa75865fa868a
voidCounter <rohitroy7014@gmail.com>
2024-09-25 13:29:23 +0600

    added some changes

--- client/api-handlers/feeds.ts
+++ client/api-handlers/feeds.ts
@@ -2,18 +2,18 @@ import {AxiosInstance} from "@/utils/AxiosInstance";
 
 export const fetchFeedItemContent = async (feedId: string) => {
     return await AxiosInstance.get(`/feed/${feedId}/content`).then((response: {
-        data: String
+        data: string
     }) => response.data)
 }
 
 export const fetchFeedItemSummary = async (feedId: string) => {
     return await AxiosInstance.get(`/feed/${feedId}/summary`).then((response: {
-        data: String
+        data: string
     }) => response.data)
 }
 
 export const fetchFeedItemInsights = async (feedId: string) => {
     return await AxiosInstance.get(`/feed/${feedId}/insights`).then((response: {
-        data: String
+        data: string
     }) => response.data)
 }
